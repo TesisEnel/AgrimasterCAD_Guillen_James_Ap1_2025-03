a3fca7cc35d7c29d372f23b87cf2854b5680aace
jamesdr-10 <james_depena@ucne.edu.do>
2025-12-01 16:12:34 -0400

    feat ✨: agrega los campos que van a tener los usuarios

--- AgrimasterCAD/Data/ApplicationUser.cs
+++ AgrimasterCAD/Data/ApplicationUser.cs
@@ -5,6 +5,11 @@ namespace AgrimasterCAD.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string NombreCompleto { get; set; } = "";
+        public string NumeroCedula { get; set; } = "";
+        public string Ciudad { get; set; } = "";
+        public string Direccion { get; set; } = "";
+        public string? NumeroCodia { get; set; }
     }
 
 }
